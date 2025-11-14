6f3f7c83a464080ce8837494b35b75d2bf1661ab
Mahmoud Mamdouh <mahmoudazizorignal@gmail.com>
2025-11-14 12:50:49 +0000

    add proper comments

--- models/B1Model.py
+++ models/B1Model.py
@@ -10,28 +10,34 @@ from models.enums import TensorBoardEnums
 
 class B1Model(nn.Module):
 
-    def __init__(self, num_classes: int, resnet_pretrained: bool):
+    def __init__(self, resnet_pretrained: bool):
         super().__init__()
         self.settings = get_settings()
-        self.num_classes = num_classes
-        self.input_size = (
-            -1, 
-            self.settings.C, 
-            self.settings.H, 
-            self.settings.W
-        )
+
+        # define the expected shape of the input of the model and the output of the model 
+        self.input_size = (-1, self.settings.C, self.settings.H, self.settings.W)
         self.target_size = (-1,)
+
+        # define the tensorboard path
         self.tensorboard_path = os.path.join(
             self.settings.TENSORBOARD_PATH,
             TensorBoardEnums.B1_TENSORBOARD_DIR.value,
         )
 
+        # wether to use the pretrained one or initialize a random one
         if resnet_pretrained:
             self.model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2)
         else:
             self.model = models.resnet50()
 
-        self.model.fc = nn.Linear(in_features=2048, out_features=num_classes, bias=True)
+        # replace the original resnet-50 head
+        self.model.fc = nn.Linear(
+            in_features=2048, 
+            out_features=self.settings.GROUP_ACTION_CNT, 
+            bias=True
+        )
+
+        # initialize the new head of the model
         torch.nn.init.normal_(self.model.fc.weight, mean=0.0, std=0.02)
     
     def forward(self, x: torch.Tensor, y: Optional[torch.Tensor] = None):
