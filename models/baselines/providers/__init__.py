6b60f4ef6842ebdaff1bb39594ebff2d748a9f3e
Mahmoud Mamdouh <mahmoudazizorignal@gmail.com>
2025-12-06 11:33:56 +0000

    fix the importing of baselines providers

--- models/baselines/providers/__init__.py
+++ models/baselines/providers/__init__.py
@@ -1,2 +1,4 @@
 from .B1ModelProvider import B1ModelProvider
 from .B3ModelProvider import B3ModelProvider
+from .B4ModelProvider import B4ModelProvider
+from .PersonModelProvider import PersonModelProvider
