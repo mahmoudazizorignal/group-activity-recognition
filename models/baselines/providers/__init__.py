bd597d805f4865b57683b07e53895accd0e3b357
mahmoudm13 <mahmoudazizorignal@gmail.com>
2025-12-20 18:56:30 +0200

    add baselines b6 & b7 to __init__

--- models/baselines/providers/__init__.py
+++ models/baselines/providers/__init__.py
@@ -2,4 +2,6 @@ from .B1ModelProvider import B1ModelProvider
 from .B3ModelProvider import B3ModelProvider
 from .B4ModelProvider import B4ModelProvider
 from .B5ModelProvider import B5ModelProvider
+from .B6ModelProvider import B6ModelProvider
+from .B7ModelProvider import B7ModelProvider
 from .PersonModelProvider import PersonModelProvider
