6a8aa73b224f1e0bd76414dafdea2080e6770212
mahmoudm13 <mahmoudazizorignal@gmail.com>
2025-12-23 18:17:23 +0200

    git pull

--- main.py
+++ main.py
@@ -1,10 +1,54 @@
-import pickle
-from helpers import Player, get_settings
+import torch
+from torch.utils.data import DataLoader
+from helpers.config import get_settings
+from models.lr.LREnums import LREnums
+from models.baselines.providers import PersonModelProvider, B5ModelProvider, B6ModelProvider, B7ModelProvider
+from models.baselines.BaselinesEnums import BaselinesEnums
+from models.datasets import DatasetProviderFactory, DatasetEnums
+from controllers import AnnotationController, TrainerController
 
+if __name__ == "__main__":
+    settings = get_settings()
+    torch.manual_seed(settings.SEED)
+    torch.cuda.manual_seed(settings.SEED)
+    annotations = AnnotationController.get_annotations(settings=settings)
+    
+    
+    
+    
+    
+    
+    import code; code.interact(local=locals())
+    base = PersonModelProvider(settings=settings, resnet_pretrained=False, temporal=True)
+    
+    # prepare the datasets of the train and val
+    train_dataset = DatasetProviderFactory(settings=settings, annotations=annotations).create(
+        provider=DatasetEnums.DatasetEnums.PERSON,
+        videos_split=settings.TRAIN_VIDEOS,
+    )
+    
+    # prepare the train and val loaders
+    train_loader = DataLoader(
+        dataset=train_dataset,
+        batch_size=settings.MINI_BATCH,
+        shuffle=True,
+    )
+    
+    # define our trainer
+    trainer = TrainerController(
+        baseline=BaselinesEnums.B8_MODEL,
+        lr_scheduler=LREnums.COSINE,
+        settings=settings,
+        train_loader=train_loader,
+        val_loader=train_loader,
+        resnet_pretrained=False,
+        base_finetuned=base,
+        base_freeze=True,
+        person_temporal=True,
+        compile=False,
+        group_only=False,
+        tensorboard_track=False,
+    )
 
-
-with open("dataset/annotations.pkl", "rb") as file:
-    group_activity_to_id, player_activity_to_id, annotations = pickle.load(file=file)
-
-settings = get_settings()
-import code; code.interact(local=locals())
+    # fit the model
+    model = trainer.fit()
